bfee321566ab291af43c2d0fa9e22872dba5b0be
ahua52 <1468492018@qq.com>
2024-02-11 12:29:36 +0000

    route aggregation

--- src/utils/tree.ts
+++ src/utils/tree.ts
@@ -1,3 +1,4 @@
+import { Permission } from '#/entity';
 import { chain } from 'ramda';
 
 /**
@@ -10,4 +11,35 @@ export function flattenTrees<T extends { children?: T[] }>(trees: T[] = []): T[]
     const children = node.children || [];
     return [node, ...flattenTrees(children)];
   }, trees);
+}
+/**
+ * nested an array containing a tree structure
+ * @param {T[]} trees - An array containing a tree structure
+ * @returns {T[]} - nested array
+ */
+export function flatToTree(flatArray: Permission[] = []): Permission[] {
+  const map: { [key: string]: Permission } = {};
+  const tree: Permission[] = [];
+
+  // Create a mapping of id to TreeNode
+  flatArray.forEach(node => {
+    if(!map[node.id]) { // TODO: 后端返回重复数据
+      map[node.id] = { ...node };
+    }
+  });
+  // Populate the children array for each node
+  for(let key in map) {
+      let node = map[key]
+      if (node.parent_id !== "") {
+          const parent = map[node.parent_id];
+          if (parent) {
+              parent.children = parent.children || [];
+              parent.children.push(map[node.id]);
+          }
+      } else {
+          tree.push(map[node.id]);
+      }
+  };
+  console.log(999999,tree)
+  return tree;
 }
